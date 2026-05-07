dacce52f90a6e155fb3bfff1e574ae1cd5f10f46
Madhuri <a00335636@student.tus.ie>
2026-05-07 10:24:10 +0100

    update initial commit

---
+++ src/main/java/com/tus/festguard/HealthController.java
@@ -0,0 +1,24 @@
+package com.tus.festguard;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of(
+                "status", "UP",
+                "app", "FestGuard",
+                "message", "Festival Pulse is running!"
+        );
+    }
+}
