204d7979cb29fa3342c25b88e5cf95d0bb2572e4
Madhuri <a00335636@student.tus.ie>
2026-05-07 10:10:37 +0100

    Initial Commit

---
+++ src/main/java/com/tus/festguard/FestGuardApplication.java
@@ -0,0 +1,13 @@
+package com.tus.festguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestGuardApplication.class, args);
+    }
+
+}
